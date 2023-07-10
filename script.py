0cb33733a9e2d249a4846b378b9d8bf9043a14b1
OgnistyPoland <32206254+OgnistyPoland@users.noreply.github.com>
2023-07-10 18:41:00 +0200

    Update script.py

--- script.py
+++ script.py
@@ -16,9 +16,9 @@ for root, directories, files in os.walk("."):
             abs_output_path = os.path.abspath(os.path.splitext(filepath)[0] + '.mp4')
             os.rename(abs_filepath, 'processing.mkv')
             if audio_channels == 2 and audio_codec != 'eac3':
-                command = f'ffmpeg -i "processing.mkv" -vf "format=yuv420p,subtitles=processing.mkv" -map_metadata -1 -movflags faststart -c:v libx264 -profile:v main -level:v 4.0 -preset veryfast -crf 17 -x264-params colormatrix=bt709 -c:a copy "{abs_output_path}"'
+                command = f'ffmpeg -i "processing.mkv" -vf "format=yuv420p,subtitles=processing.mkv" -map_metadata -1 -movflags faststart -c:v libx264 -profile:v main -level:v 4.1 -preset slow -crf 16 -x264-params colormatrix=bt709 -c:a copy "{abs_output_path}"'
             elif audio_channels == 6 or audio_codec == 'eac3':
-                command = f'ffmpeg -i "processing.mkv" -vf "format=yuv420p,subtitles=processing.mkv" -map_metadata -1 -movflags faststart -c:v libx264 -profile:v main -level:v 4.0 -preset veryfast -crf 17 -x264-params colormatrix=bt709 -c:a libfdk_aac -b:a 256k -ac 2 "{abs_output_path}"'
+                command = f'ffmpeg -i "processing.mkv" -vf "format=yuv420p,subtitles=processing.mkv" -map_metadata -1 -movflags faststart -c:v libx264 -profile:v main -level:v 4.1 -preset slow -crf 16 -x264-params colormatrix=bt709 -c:a libfdk_aac -b:a 256k "{abs_output_path}"'
             else:
                 print(f"{file} - incorrect channel count ({audio_channels})")
                 continue
